d7bca4600132c946afcb8893f7c5787b9b789c16
delgoden <delgoden@gmail.com>
2021-10-04 13:20:34 +0300

    Added functions FindPaymentByID and Reject in package stats and tests for him

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -3,6 +3,7 @@ package wallet
 import (
 	"fmt"
 	"github.com/delgoden/wallet/pkg/types"
+	"github.com/google/uuid"
 	"reflect"
 	"testing"
 )
@@ -70,7 +71,7 @@ func TestService_FindAccountByID_success(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(account, got) {
-		t.Errorf(("FindAccountByID(): wrong account returned = %v"), err)
+		t.Errorf("FindAccountByID(): wrong account returned = %v", err)
 		return
 	}
 }
@@ -85,6 +86,107 @@ func TestService_FindAccountByID_failed(t *testing.T) {
 	_, err = s.FindAccountByID(10)
 	if err != ErrAccountNotFound {
 		t.Errorf("FindAccountByID(): error = %v", err)
+		return
+	}
+
+}
+
+func TestService_FindPaymentByID_success(t *testing.T) {
+	s := newTestService()
+	_, payments, err := s.addAccount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	payment := payments[0]
+	got, err := s.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("FindPaymentByID(): error = %v", err)
+		return
+	}
+
+	if !reflect.DeepEqual(payment, got) {
+		t.Errorf("FindPaymentByID(): wrong payment returned = %v", err)
+		return
+	}
+
+}
+
+func TestService_FindPaymentByID_fail(t *testing.T) {
+	s := newTestService()
+	_, _, err := s.addAccount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	_, err = s.FindPaymentByID(uuid.New().String())
+	if err == nil {
+		t.Errorf("FindPaymentByID(): must return error, returned nil")
+		return
+	}
+
+	if err != ErrPaymentNotFound {
+		t.Errorf("FindPaymentByID(): must return ErrPaymentNotFound, returned = %v", err)
+		return
+	}
+
+}
+
+func Test_Reject_success(t *testing.T) {
+	s := newTestService()
+	_, payments, err := s.addAccount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	payment := payments[0]
+	err = s.Reject(payment.ID)
+	if err != nil {
+		t.Errorf("Reject(): error = %v", err)
+		return
+	}
+
+	savedPayment, err := s.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("Reject(): can't find payment by id, error = %v", err)
+		return
+	}
+
+	if savedPayment.Status != types.PaymentStatusFail {
+		t.Errorf("Reject(): status didn't changed, payment = %v", savedPayment)
+		return
+	}
+
+	savedAccount, err := s.FindAccountByID(payment.AccountID)
+	if err != nil {
+		t.Errorf("Reject(): can't find account by id, error = %v", err)
+		return
+	}
+
+	if savedAccount.Balance != defaultTestAccount.balance {
+		t.Errorf("Reject(): balance didn't changed, account = %v", savedAccount)
+		return
+	}
+}
+
+func Test_Reject_fail(t *testing.T) {
+	s := newTestService()
+	_, _, err := s.addAccount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = s.Reject(uuid.New().String())
+	if err == nil {
+		t.Errorf("Reject():  must return error, returned nil")
+		return
+	}
+	if err != ErrPaymentNotFound {
+		t.Errorf("FindPaymentByID(): must return ErrPaymentNotFound, returned = %v", err)
 		return
 	}
 
